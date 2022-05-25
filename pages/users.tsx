f7c5eda41151ffabfd24e97078fde12056e5f4f4
N!9r4 <KiniGra.665@gmail.com>
2022-05-25 19:56:51 +0200

    Created user card in html

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,7 +1,65 @@
 import { NextPage } from "next";
+import { redirect } from "next/dist/server/api-utils";
+import styles from "../styles/Users.module.css";
 
 const Users: NextPage = () => {
-  return <h1>Not implemented</h1>;
+  return (
+    <div className={styles.bg}>
+       <div className={styles.card}>
+            
+          <div className={styles.personal}>
+
+            <div className={styles.img}></div>
+
+            <div className={styles.info}>
+
+              <div className={styles.name}>
+                <p>Ronald Richards</p>
+                <p>ronald.richards@example.com</p>
+              </div>
+
+              <div className={styles.genderAge}>
+                <p>male</p>
+                <p>34</p>
+              </div>
+            </div>
+
+          </div>
+
+          <div className={styles.address}>
+            
+            <div className={styles.text}>
+              <p>Crown Point</p>
+              <p>Street</p>
+            </div>
+
+            <div className={styles.line}></div>
+
+            <div className={styles.text}>
+              <p>Austin</p>
+              <p>City</p>
+            </div>
+
+            <div className={styles.line}></div>
+            
+            <div className={styles.text}>
+              <p>73301</p>
+              <p>Zip code</p>
+            </div>
+
+          </div>
+
+          <ul className={styles.images}>
+            <li>Image 1</li>
+            <li></li>
+            <li>Image 2</li>
+            <li></li>
+            <li>Image 3</li>
+          </ul>
+
+      </div>
+    </div>
+  )
 };
 
 export default Users;
